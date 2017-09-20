1fa3dca8fedca62ada4c3b22e71c9d1b3dd65fa9
Harry Vuong <harry_95_v@hotmail.com>
2017-09-20 23:24:46 +0200

    Refactored addItem into loadItem and addNewItem

--- popup.js
+++ popup.js
@@ -14,13 +14,7 @@
 var listItemTemplate = document.getElementById('listItem-template');
 
 // Store all label images in array so that functions can later randomly select a label
-var imagesArray = ['label blue.png', 'label green.png', 'label purple.png', 'label red.png', 'label yellow.png'];
-
-// Keep track of previous randomly generated number
-// Start with a number that is larger than the amount of labels just to guarantee that
-// the first label can be one of the labels in the imagesArray
-var prevRandomNum = 100;
-
+var labelArray = ['label blue.png', 'label green.png', 'label purple.png', 'label red.png', 'label yellow.png'];
 
 // Update number of list items
 function updateNumberOfListItems(){
@@ -38,6 +32,7 @@ function updateNumberOfListItems(){
             ' items to read';
     });
 }
+
 
 /**
  * Get the current URL.
@@ -85,7 +80,7 @@ function getCurrentTabUrl(callback) {
             }
 
             if(!flag){
-                callback(title, url, '4.45 AM', true, '', '');
+                callback(title, url, '4.45 AM');
             }
             else{
                 // A Modal should appear to inform user
@@ -110,59 +105,31 @@ function getCurrentTabUrl(callback) {
  * @param {string} title - Title of tab
  * @param {string} url - Url of the tab
  * @param {string} timeStamp - Time the item was added
- * @param {boolean} toStore - Boolean to store or not to store list item
  * @param {string} label - Label to load for the list item
  * @param {string} key - The ID for the listitem
  */
-function addItem(title, url, timeStamp, toStore, label, key){
-    // The id of the listitem
-    var id;
+function loadItems(title, url, timeStamp, label, key){
     // Clone the list item template so the function doesn't overwrite the original template
     var listItem = listItemTemplate.content.cloneNode(true);
 
-    // Check if the id already exists
-    // if not then generate a new id
-    if(key !== ''){
-        id = key;
-    }
-    else{
-        // Get current date since 1 January 1970 00:00:00 UTC in milliseconds
-        id = Date.now().toString();
-    }
-
     /*var thumbNailCb = function(dataUrl){
         if(dataUrl === null || dataUrl === ''){
-            var randomNum = Math.floor(Math.random() * (imagesArray.length+1));
-            listItem.querySelector('.label').src = './assets/' + imagesArray[randomNum];
+            var randomNum = Math.floor(Math.random() * (labelArray.length+1));
+            listItem.querySelector('.label').src = './assets/' + labelArray[randomNum];
         }
         else {
             console.log(listItem.querySelector('.label'));
-            var randomNum = Math.floor(Math.random() * (imagesArray.length+1));
-            listItem.querySelector('.label').src = './assets/' + imagesArray[randomNum];
+            var randomNum = Math.floor(Math.random() * (labelArray.length+1));
+            listItem.querySelector('.label').src = './assets/' + labelArray[randomNum];
             //listItem.querySelector('.label').src = dataUrl;
         }
     };*/
 
     // Set the id of the listitem
-    listItem.querySelector('.listItems').setAttribute('id', id);
+    listItem.querySelector('.listItems').setAttribute('id', key);
 
-
-    // Set the thumbnail of current tab
-    // Make sure that no labels can be of the same color consecutively
-    //chrome.tabs.captureVisibleTab(thumbNailCb);
-    var currentRandomNum = Math.floor(Math.random() * (imagesArray.length));
-    while(prevRandomNum === currentRandomNum){
-        currentRandomNum = Math.floor(Math.random() * (imagesArray.length));
-    }
-    prevRandomNum = currentRandomNum;
-    var tempLabel = imagesArray[currentRandomNum];
-    // Check whether to load the label from storage or set a new random label
-    if(label !== ''){
-        listItem.querySelector('.label').src = './assets/' + label;
-    }
-    else {
-        listItem.querySelector('.label').src = './assets/' + tempLabel;
-    }
+    // Set the label of the listitem
+    listItem.querySelector('.label').src = './assets/' + label;
 
     // Set the title of the current tab
     // 117 characters corresponds approximately to 5 rows at most
@@ -182,32 +149,11 @@ function addItem(title, url, timeStamp, toStore, label, key){
     // Set the time of when adding the article
     listItem.querySelector('.timeStamp').innerHTML = timeStamp;
 
-
-    // This conditional statement will only be executed when the user
-    // is adding a new item
-    // Set the ID of the new listitem
-    // and store the new item locally in the computer to make it persistent
-    if(toStore){
-        // Set ID to listItem
-        listItem.querySelector('.listItems').setAttribute('id', id);
-        var dataObj = {};
-        dataObj[id] = {
-            'title': title,
-            'url': url,
-            'label': tempLabel};
-        chrome.storage.local.set(dataObj, function() {
-            // Notify that we saved.
-            // Modal should appear in the page action rather than in browser action
-            console.log('Settings saved');
-        });
-        updateNumberOfListItems();
-    }
-
     // Add click event to delete listitem
     listItem.querySelector('.deleteIcon').addEventListener('click', function(){
-        var listElement = document.getElementById(id);
+        var listElement = document.getElementById(key);
         listElement.parentNode.removeChild(listElement);
-        chrome.storage.local.remove(id, function(){
+        chrome.storage.local.remove(key, function(){
             // Notify that successfully removed
             console.log('Deletion successful!');
             updateNumberOfListItems();
@@ -216,6 +162,88 @@ function addItem(title, url, timeStamp, toStore, label, key){
 
     // Add the list item to the DOM
     document.getElementById('container').appendChild(listItem);
+}
+
+/**
+ * Add reading items to listItem
+ *
+ * @param {string} title - Title of tab
+ * @param {string} url - Url of the tab
+ * @param {string} timeStamp - Time the item was added
+ */
+function addNewItem(title, url, timeStamp){
+    chrome.storage.local.get(null, function(objects){
+        // Clone the list item template so the function doesn't overwrite the original template
+        var listItem = listItemTemplate.content.cloneNode(true);
+
+        var lastItemLabel;
+        // Get the label of the last list item
+        for(var key in objects){
+            if(!objects.hasOwnProperty(key))
+                continue;
+
+            lastItemLabel = objects[key].label;
+        }
+
+        // Get current date since 1 January 1970 00:00:00 UTC in milliseconds
+        var id = Date.now().toString();
+
+        // Set ID of the listitem
+        listItem.querySelector('.listItems').setAttribute('id', id);
+
+        //
+        var currentRandomNum = Math.floor(Math.random() * (labelArray.length));
+        while(labelArray.indexOf(lastItemLabel) === currentRandomNum){
+            currentRandomNum = Math.floor(Math.random() * (labelArray.length));
+        }
+        listItem.querySelector('.label').src = './assets/' + labelArray[currentRandomNum];
+
+        // Set the title of the current tab
+        // 117 characters corresponds approximately to 5 rows at most
+        var titleOfArticleNode = listItem.querySelector('.titleOfArticle');
+        if(title.length > 117){
+            titleOfArticleNode.innerHTML = title.substring(0,117) + '...';
+            titleOfArticleNode.setAttribute('title', title);
+        }
+        else {
+            titleOfArticleNode.innerHTML = title;
+        }
+        // Make title of list item clickable so it can redirect user to the url
+        titleOfArticleNode.addEventListener('click', function(){
+            window.open(url,'_blank');
+        });
+
+        // Set the time of when adding the article
+        listItem.querySelector('.timeStamp').innerHTML = timeStamp;
+
+        // Set the ID of the new listitem
+        // and store the new item locally in the computer to make it persistent
+        var dataObj = {};
+        dataObj[id] = {
+            'title': title,
+            'url': url,
+            'label': labelArray[currentRandomNum]};
+        chrome.storage.local.set(dataObj, function() {
+            updateNumberOfListItems();
+            // Notify that we saved.
+            // Modal should appear in the page action rather than in browser action
+            console.log('Settings saved');
+        });
+
+        // Add click event to delete listitem
+        listItem.querySelector('.deleteIcon').addEventListener('click', function(){
+            var listElement = document.getElementById(id);
+            listElement.parentNode.removeChild(listElement);
+            chrome.storage.local.remove(id, function(){
+                // Notify that successfully removed
+                console.log('Deletion successful!');
+                updateNumberOfListItems();
+            });
+        });
+
+        // Add the list item to the DOM
+        document.getElementById('container').appendChild(listItem);
+    });
 }
 
 // Listen for when html content has been loaded to render list items
@@ -228,7 +256,7 @@ document.addEventListener('DOMContentLoaded', function() {
                 continue;
 
             var obj = objects[key];
-            addItem(obj.title, obj.url, '4.45 AM', false, obj.label, key);
+            loadItems(obj.title, obj.url, '4.45 AM', obj.label, key);
         }
         updateNumberOfListItems();
     });
@@ -237,6 +265,6 @@ document.addEventListener('DOMContentLoaded', function() {
 // Listen for command to add reading item
 chrome.commands.onCommand.addListener(function(command) {
     if(command === 'toggle-feature-addItem'){
-        getCurrentTabUrl(addItem);
+        getCurrentTabUrl(addNewItem);
     }
 });
